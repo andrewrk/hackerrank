09e9a164a3f75d5d7c386373feecc3509b1ac179
Andrew Kelley <superjoe30@gmail.com>
2013-03-29 13:05:09 -0400

    botclean simple solution

--- botclean/main.go
+++ botclean/main.go
@@ -15,49 +15,96 @@ type pos struct {
 }
 
 type world struct {
-	botPos pos
+	botPos * pos
 	cells [][]bool
 }
 
 func parsePos(line string) (*pos, error) {
 	slice := strings.Split(strings.TrimSpace(line), " ")
 	// flip x and y
-	result := new(pos)
 	x64, err := strconv.ParseInt(slice[1], 10, 32)
 	if err != nil { return nil, err }
 	y64, err := strconv.ParseInt(slice[0], 10, 32)
 	if err != nil { return nil, err }
-	result.x = int(x64)
-	result.y = int(y64)
-	return result, nil
+	return &pos{int(x64), int(y64)}, nil
 }
 
-func readWorld(reader io.Reader) (*world, error) {
+func readWorld(stream io.Reader) (*world, error) {
 	reader := bufio.NewReader(stream)
 	botPosLine, err := reader.ReadString('\n')
 	if err != nil { panic(err) }
-	botPos, err := parsePos(botPosLine)
+	w := new(world)
+	w.botPos, err = parsePos(botPosLine)
 	if err != nil { panic(err) }
-	grid := make([][]bool, 0, 100)
+	w.cells = make([][]bool, 0, 100)
 	eof := false
-	for y := 0; !eof; y++ {
+	for !eof {
 		line, err := reader.ReadString('\n')
+		line = strings.TrimSpace(line)
 		if err == io.EOF {
 			eof = true
 		} else if err != nil {
 			panic(err)
 		}
 		row := make([]bool, len(line), len(line))
-		grid = append(grid, row)
+		w.cells = append(w.cells, row)
 		for x, char := range line {
 			if char == 'd' {
 				row[x] = true
 			}
 		}
 	}
+	return w, nil
+}
+
+type posMatch struct {
+	p * pos
+	distance int
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+func rectilinearDist(pos1 * pos, pos2 * pos) int {
+	return abs(pos1.x - pos2.x) + abs(pos1.y - pos2.y)
 }
 
 func main() {
 	w, err := readWorld(os.Stdin)
 	if err != nil { panic(err) }
+	// always clean if we're in a dirty cell
+	if w.cells[w.botPos.y][w.botPos.x] {
+		fmt.Println("CLEAN")
+		return;
+	}
+	// simple strategy: go for the closest dirty spot
+	closest := new(posMatch)
+	var it pos
+	for it.y = 0; it.y < len(w.cells); it.y++ {
+		for it.x = 0; it.x < len(w.cells[it.y]); it.x++ {
+			if w.cells[it.y][it.x] {
+				// dirty
+				dist := rectilinearDist(&it, w.botPos)
+				if closest.p == nil || dist < closest.distance {
+					var clone pos = it
+					closest.p = &clone
+					closest.distance = dist
+				}
+			}
+		}
+	}
+	if w.botPos.x < closest.p.x {
+		fmt.Println("RIGHT")
+	} else if w.botPos.x > closest.p.x {
+		fmt.Println("LEFT")
+	} else if w.botPos.y < closest.p.y {
+		fmt.Println("DOWN")
+	} else if w.botPos.y > closest.p.y {
+		fmt.Println("UP")
+	}
 }
+

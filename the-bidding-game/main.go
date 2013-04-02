cecf3b4fa9c636725f9c3c3d72cf1997630a8419
Andrew Kelley <superjoe30@gmail.com>
2013-04-02 04:24:45 -0400

    bidding game: simple strategy that doesn't perform very well

--- the-bidding-game/main.go
+++ the-bidding-game/main.go
@@ -1,133 +1,96 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+	"bufio"
+	"os"
+	"io"
+	"strings"
+)
 
 type Game struct {
-	playerCash [2]int
-	playerState [2]int
+	cash [2]int
+	bottlePos int
 	drawAdvantage int
 }
 
 var otherPlayer = [2]int{1, 0}
-var playerName = [2]string{"P1", "P2"}
-const PRINT = false
+var playerMoveDelta = [2]int{-1, 1}
 
 func NewGame() Game {
-	return Game{[2]int{25, 25}, [2]int{2, 2}, 0}
+	return Game{[2]int{100, 100}, 5, 0}
 }
 
-func (g * Game) next(bets [2]int) {
-	if PRINT {
-		fmt.Println("P1State", g.playerState[0], "P2State", g.playerState[1], "drawAdv", playerName[g.drawAdvantage], "P1Cash", g.playerCash[0], "P2Cash", g.playerCash[1], "P1Bid", bets[0], "P2Bid", bets[1])
-	}
+func (g * Game) next(bids [2]int) {
 	var winner int
-	if bets[0] > bets[1] {
+	if bids[0] > bids[1] {
 		winner = 0
-	} else if bets[1] > bets[0] {
+	} else if bids[1] > bids[0] {
 		winner = 1
 	} else {
 		winner = g.drawAdvantage
 		g.drawAdvantage = otherPlayer[winner]
 	}
-	g.playerCash[winner] -= bets[winner]
-	g.playerState[winner] -= 1
-}
-
-func (g * Game) computeWinner() int {
-	// the player who has not moved will try to minimally
-	// match the other player's bet
-	loser := 0
-	if g.playerState[0] < g.playerState[1] { loser = 1 }
-	winner := otherPlayer[loser]
-	winnerBet := g.playerCash[winner]
-	loserBet := winnerBet
-	if g.drawAdvantage != loser { loserBet += 1 }
-
-	// we already know that the winner of the first bet
-	// won the game
-	if loserBet > g.playerCash[loser] {
-		return winner
-	}
-
-	// need to do another round of bidding to find out
-	var bets [2]int
-	bets[loser] = loserBet
-	bets[winner] = winnerBet
-	g.next(bets)
-
-	if g.playerState[0] == 0 { return 0 }
-	if g.playerState[1] == 0 { return 1 }
-
-	if g.playerState[0] != 1 && g.playerState[1] != 1 {
-		if g.playerState[0] == 2 && g.playerState[1] == 2 {
-			if g.playerCash[0] > g.playerCash[1] {
-				return 0
-			} else if g.playerCash[1] > g.playerCash[0] {
-				return 1
-			} else {
-				return g.drawAdvantage
-			}
-		} else {
-			fmt.Println(g)
-			panic("bad step")
-		}
-	}
-
-	// both players bet everything they have
-	g.next(g.playerCash)
-
-	if g.playerState[0] == 0 { return 0 }
-	if g.playerState[1] == 0 { return 1 }
-
-	// both players bet everything they have
-	g.next(g.playerCash)
-
-	if g.playerState[0] == 0 { return 0 }
-	if g.playerState[1] == 0 { return 1 }
-
-	panic("bad step")
-}
-
-func (originalGame Game) computeWinner2() int {
-	var wins [2][101]int
-	for p1bet := 1; p1bet <= originalGame.playerCash[0]; p1bet++ {
-		for p2bet := 1; p2bet <= originalGame.playerCash[1]; p2bet++ {
-			// set up this game
-			g := originalGame
-			// do the first bet
-			g.next([2]int{p1bet, p2bet})
-			// from there we can figure out who wins
-			winner := g.computeWinner()
-			winnerBet := p1bet
-			if winner == 1 { winnerBet = p2bet }
-			fmt.Print(winner)
-			wins[winner][winnerBet] += 1
-			//fmt.Println("P1Bet", p1bet, "P2Bet", p2bet, "winner", playerName[winner])
-		}
-		fmt.Print("\n")
-	}
-	for i := 0; i < 2; i++ {
-		for bet := 1; bet <= originalGame.playerCash[otherPlayer[i]]; bet++ {
-			if wins[i][bet] == originalGame.playerCash[otherPlayer[i]] {
-				return i
-			}
-		}
-	}
-	return 2
+	g.cash[winner] -= bids[winner]
+	g.bottlePos += playerMoveDelta[winner]
 }
 
 func main() {
-	// set up every possible game
-	originalGame := NewGame()
-	originalGame.computeWinner2()
-	return
-	for p1bet := 1; p1bet <= originalGame.playerCash[0]; p1bet++ {
-		for p2bet := 1; p2bet <= originalGame.playerCash[1]; p2bet++ {
-			g := originalGame
-			g.next([2]int{p1bet, p2bet})
-			w := g.computeWinner2()
-			fmt.Print(w)
-		}
-		fmt.Print("\n")
+	reader := bufio.NewReader(os.Stdin)
+	playerLine, err := reader.ReadString('\n')
+	if err != nil { panic(err) }
+	scotchPosLine, err := reader.ReadString('\n')
+	if err != nil { panic(err) }
+	p1BidsLine, err := reader.ReadString('\n')
+	if err != nil { panic(err) }
+	p2BidsLine, err := reader.ReadString('\n')
+	if err != io.EOF && err != nil { panic(err) }
+	scotchPos64, err := strconv.ParseInt(strings.TrimSpace(scotchPosLine), 10, 32)
+	if err != nil { panic(err) }
+	player64, err := strconv.ParseInt(strings.TrimSpace(playerLine), 10, 32)
+	if err != nil { panic(err) }
+	scotchPos := int(scotchPos64)
+	player := int(player64) - 1
+	bids := [2][]int{
+		toIntSlice(strings.Fields(p1BidsLine)),
+		toIntSlice(strings.Fields(p2BidsLine)),
 	}
+	g := NewGame()
+	for i := range(bids[0]) {
+		g.next([2]int{bids[0][i], bids[1][i]})
+	}
+	if g.bottlePos != scotchPos {
+		fmt.Println("scotchPos", scotchPos, "g.bottlePos", g.bottlePos)
+		panic("bad step")
+	}
+	playerPos := [2]int{0, 10}
+	delta := abs(playerPos[player] - scotchPos)
+	if delta == 1 || g.cash[player] == 0 {
+		fmt.Println(g.cash[player])
+		return
+	}
+	myBid := g.cash[player] / (delta * 3 / 2)
+	if myBid < 1 {
+		myBid = 1
+	}
+	fmt.Println(myBid)
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+
+func toIntSlice(slice []string) []int {
+	intValues := make([]int, len(slice))
+	for i, numStr := range(slice) {
+		result, err := strconv.ParseInt(strings.TrimSpace(numStr), 10, 32)
+		if err != nil { panic(err) }
+		intValues[i] = int(result)
+	}
+	return intValues
 }

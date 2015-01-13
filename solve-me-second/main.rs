39f1b19d56a4be3559553e3b39a5592abc8ab4d1
Andrew Kelley <superjoe30@gmail.com>
2015-01-13 16:27:08 -0500

    hackerrank likes this style of solve-me-second better

--- solve-me-second/main.rs
+++ solve-me-second/main.rs
@@ -1,21 +1,16 @@
-#![allow(unstable)]
+fn add(x: usize, y: usize) -> usize {
+    x + y
+}
 
 fn main() {
     let mut stdin = std::io::stdio::stdin();
-
-    // discard the redundant first line
-    stdin.read_line();
-
-    loop {
-        match stdin.read_line() {
-            Ok(line) => {
-                let mut sum:isize = 0;
-                for x in line.split(' ') {
-                    sum += x.trim().parse().unwrap();
-                }
-                println!("{}", sum);
-            }
-            Err(e) => break
-        }
+    let line = stdin.read_line().ok().unwrap();
+    let count = line.trim().parse().unwrap();
+    for _ in range(0, count) {
+        let line = stdin.read_line().ok().unwrap();
+        let v: Vec<&str> = line.trim().split(' ').collect();
+        let x = v[0].parse().unwrap();
+        let y = v[1].parse().unwrap();
+        println!("{}", add(x, y));
     }
 }
